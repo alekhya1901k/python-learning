9337e29526c23aa7a6589a2f707b54cf2d1211ae
Alekhya <alekhya1901k@gmail.com>
2026-06-30 14:57:06 -0500

    day 18 a

--- core-concepts/Day18_Prime Number Checker/main.py
+++ core-concepts/Day18_Prime Number Checker/main.py
@@ -1,14 +1,16 @@
-def is_prime(number):
-    if number <= 1:
+def is_prime(num):
+    # numbers less than or equal to 1 are not prime
+    if num <= 1:
         return False
 
-    for i in range(2, int(number ** 0.5) + 1):
-        if number % i == 0:
+    # check divisors from 2 up to square root of num
+    for i in range(2, int(num ** 0.5) + 1):
+        # if num is divisible by i, it is not prime
+        if num % i == 0:
             return False
 
+    # if no divisors found, it is prime
     return True
-
-
 n = int(input("Enter a number: "))
 print(is_prime(n))
 
